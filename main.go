82da0ff3f3ccef372375c2c1581991262ff4e4f7
Hebici <2257714950@qq.com>
2023-12-15 21:24:17 +0800

    internal

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"Awesome/component"
+	"time"
 )
 
 func main() {
 	supervisers := component.Start(1)
 	for i := range supervisers {
 		go supervisers[i].PrintInfo()
+	}
+	for {
+		time.Sleep(time.Second * 1)
 	}
 }
